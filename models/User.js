a04f167d754f0728bab6a84e9e365dffafebea0f
KCaseyChamberlain <caseygchamberlain@gmail.com>
2022-04-15 22:58:59 -0600

    User model rough draft created

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: truez
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thoughts'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Friends'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        // prevents virtuals from creating duplicate of _id as `id`
+        id: false
+    }
+);
+
+// get total count of comments and replies on retrieval
+UserSchema.virtual('friendCount').get(function () {
+    return this.comments.reduce(
+        (total, comment) => total + comment.replies.length + 1,
+        0
+    );
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
